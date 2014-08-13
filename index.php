73dfdb51faa525d877eaa97fa724dedbdb77fb13
wolfjonny99 <wolfjonny@gmx.de>
2014-08-13 22:28:38 +0200

    added description to the index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>Stundenplan Abfrage</title>
+</head>
+<body>
+	<h1>Stundenplan Abfrage</h1>
+	<p>choose the teacher, who's timetable you want to see.</p>
+</body>
+</html>
